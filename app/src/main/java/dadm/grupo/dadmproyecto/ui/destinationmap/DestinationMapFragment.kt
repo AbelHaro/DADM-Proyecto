dd414822c4e7a980ce97127064c0f3caa21b6893
Abel Haro <ahararm@upv.es>
2025-03-26 19:34:38 +0100

    Fragments del mainActivity

---
+++ app/src/main/java/dadm/grupo/dadmproyecto/ui/destinationmap/DestinationMapFragment.kt
@@ -0,0 +1,28 @@
+package dadm.grupo.dadmproyecto.ui.destinationmap
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import dadm.grupo.dadmproyecto.R
+import dadm.grupo.dadmproyecto.databinding.FragmentLoginBinding
+
+class DestinationMapFragment : Fragment(R.layout.fragment_destination_map) {
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/dadm/grupo/dadmproyecto/ui/ranking/RankingFragment.kt
@@ -0,0 +1,28 @@
+package dadm.grupo.dadmproyecto.ui.ranking
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import dadm.grupo.dadmproyecto.R
+import dadm.grupo.dadmproyecto.databinding.FragmentLoginBinding
+
+class RankingFragment : Fragment(R.layout.fragment_ranking) {
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
